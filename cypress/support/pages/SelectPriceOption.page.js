32f9bb241907c5e2a23db4de20b6dc3539f61b16
Caroline Ferraz <projetos.crrz@gmail.com>
2025-01-18 11:51:15 -0300

    Feat: Adicionando automatizações da aba Select Price Option

---
+++ cypress/support/pages/SelectPriceOption.page.js
@@ -0,0 +1,7 @@
+const ABA_SELECT_PRICE_OPTION = '#selectpriceoption'
+const LBL_SELECT_PRICE_OPTION = 'tr:has(th:contains("Select Option")) > .group > :nth-child(1)'
+
+Cypress.Commands.add('preencheCamposSelectPriceOption', () => {
+    cy.get(ABA_SELECT_PRICE_OPTION).click()
+    cy.get(LBL_SELECT_PRICE_OPTION).click()
+})
