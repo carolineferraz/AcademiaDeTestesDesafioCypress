ac2afee38c8fab6db4ddc6948e98a4938c7eb3e8
Caroline Ferraz <projetos.crrz@gmail.com>
2025-01-18 13:04:23 -0300

    Fix: Aumentando o timeout para recebimento do objeto da resposta da requisição http post após submeter o formulário

--- cypress/support/pages/SendQuote.page.js
+++ cypress/support/pages/SendQuote.page.js
@@ -7,22 +7,18 @@ const BTN_SEND_EMAIL = '#sendemail'
 
 Cypress.Commands.add('preencheCamposSendQuote', () => {
     cy.get(ABA_SEND_QUOTE).click()
-    cy.get(INPUT_EMAIL).type('caroline@email.com')
+    cy.get(INPUT_EMAIL).type('caroline7@email.com')
     cy.get(INPUT_USERNAME).type('caroline')
     cy.get(INPUT_PASSWORD).type('qwE123#')
     cy.get(INPUT_CONFIRM_PASSWORD).type('qwE123#')
 })
 
 Cypress.Commands.add('submeterFomulario', () => {
-    // Intercepta a requisição POST
     cy.intercept('POST', '/101/tcpdf/pdfs/quote.php').as('postQuote')
 
-    // Clica no botão para enviar o formulário
     cy.get(BTN_SEND_EMAIL).click()
 
-    // Espera pela resposta da requisição POST
-    cy.wait('@postQuote').then((interception) => {
-        // Verifica se a resposta foi bem-sucedida
+    cy.wait('@postQuote', { timeout: 60000 }).then((interception) => {
         expect(interception.response.statusCode).to.equal(200)
     })
 })
