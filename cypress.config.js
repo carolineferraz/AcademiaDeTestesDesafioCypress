4685e7b76a2dd9952cf85f0abc4e881b07ce8d06
Caroline Ferraz <projetos.crrz@gmail.com>
2025-01-18 11:40:18 -0300

    Fix: Ajuste na baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     viewportHeight: 720,
     watchForFileChanges: false,
     specPattern: 'cypress/e2e/**/*.feature',
-    baseUrl: 'http://www.automationpractice.pl/',
+    baseUrl: 'http://sampleapp.tricentis.com/101',
 
     setupNodeEvents(on, config) {
       const cucumber = require('cypress-cucumber-preprocessor').default;
